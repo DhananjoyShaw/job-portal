26fa71ef18495f826a896e3698089eb30aa9e01d
DhananjoyShaw <dhananjoyshaw0218@gmail.com>
2025-04-21 20:25:31 +0530

    feat: implement browse search job functionality

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel';
 import { Button } from './ui/button';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setSearchedQuery } from '@/redux/jobSlice';
 
 const category = [
     "Frontend Developer",
@@ -11,6 +14,14 @@ const category = [
 ]
 
 const CategoryCarousel = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const searchJobHandler = (query) => {
+        dispatch(setSearchedQuery(query));
+        navigate("/browse");
+    }
+
     return (
         <div>
             <Carousel className="w-full max-w-xl mx-auto my-12">
@@ -20,7 +31,7 @@ const CategoryCarousel = () => {
                             <CarouselItem
                                 key={index}
                                 className="md:basis-[35%] lg-basis-1/3">
-                                <Button variant="outline" className="rounded-full bg-gray-200">{cat}</Button>
+                                <Button onClick={() => searchJobHandler(cat)} variant="outline" className="rounded-full bg-gray-200">{cat}</Button>
                             </CarouselItem>
                         ))
                     }
