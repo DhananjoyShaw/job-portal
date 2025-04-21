8dd0731f2d7f6723969462bf59e69ab7d45f62ff
DhananjoyShaw <dhananjoyshaw0218@gmail.com>
2025-04-21 21:58:00 +0530

    feat: implement filter jobs functionality

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,38 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { RadioGroup, RadioGroupItem } from './ui/radio-group'
 import { Label } from './ui/label'
+import { useDispatch } from 'react-redux'
+import { setSearchedQuery } from '@/redux/jobSlice'
 
 const fitlerData = [
     {
         fitlerType: "Location",
-        array: ["Delhi NCR", "Bangalore", "Hyderabad", "Pune", "Mumbai"]
+        array: ["Bangalore", "Noida", "Hyderabad", "Pune", "Mumbai"]
     },
     {
         fitlerType: "Industry",
-        array: ["Frontend Developer", "Backend Developer", "FullStack Developer"]
+        array: ["Frontend Developer", "Backend Developer", "Full Stack Developer", "Data Scientist", "Data Analyst"]
     },
     {
         fitlerType: "Salary",
-        array: ["0-40k", "42-1lakh", "1lakh to 5lakh"]
+        array: ["0-5 Lakh", "0-10 Lakh", "10-20 Lakh"]
     },
 ]
 
 const FilterCard = () => {
+    const [selectedValue, setSelectedValue] = useState('');
+    const dispatch = useDispatch();
+
+    const changeHandler = (value) => {
+        setSelectedValue(value);
+    }
+
+    useEffect(() => {
+        // console.log("Selected Value: ", selectedValue);
+        dispatch(setSearchedQuery(selectedValue));
+    }, [selectedValue]);
+
     return (
         <div className='w-full bg-white p-3 rounded-md'>
             <h1 className='font-bold text-lg'>Filter Jobs</h1>
             <hr className='mt-3' />
-            <RadioGroup >
+            <RadioGroup value={selectedValue} onValueChange={changeHandler} >
                 {
                     fitlerData.map((data, index) => (
                         <div>
                             <h1 className='font-bold text-lg'>{data.fitlerType}</h1>
                             {
                                 data.array.map((item, idx) => {
+                                    const itemId = `id${index}-${idx}`
                                     return (
                                         <div className='flex items-center space-x-2 my-2'>
-                                            <RadioGroupItem value={item} />
-                                            <Label >{item}</Label>
+                                            <RadioGroupItem value={item} id={itemId} />
+                                            <Label htmlFor={itemId}>{item}</Label>
                                         </div>
                                     )
                                 })
